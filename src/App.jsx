36d04e064289d5bd65d9edaf2af48f20f4bad96c
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2024-08-20 13:57:17 +0200

    added constants arrays and icon imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import items from "./constants/items_array";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  console.log(items[0].name);
   return (
     <>
       <h1>Bingoholmen</h1>
---
+++ src/constants/icons_import.js
@@ -0,0 +1,8 @@
+import IcecreamIcon from "@mui/icons-material/Icecream"; //Ice Cream
+import ApartmentIcon from "@mui/icons-material/Apartment"; //Building
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar"; //Car
+import DirectionsBoatIcon from "@mui/icons-material/DirectionsBoat"; //Boat
+import PetsIcon from "@mui/icons-material/Pets"; //Animal Paw
+import CachedIcon from "@mui/icons-material/Cached"; //Shuffle icon
+import SchoolIcon from "@mui/icons-material/School"; //Hat Icon
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline"; //Help/ About
